c33b09b3f67fbe4bdacefdb07498776ce623c858
Guilhem PECH <guilhem.pech@gmail.com>
2024-05-26 15:54:52 +0200

    Use templated method instead of generic to cover more Collection types

--- Code/Utils/ExtensionMethods.cs
+++ Code/Utils/ExtensionMethods.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 
 namespace Sandbox.Utils;
 
 // Useful extension methods for various types
 public static class ExtensionMethods
 {
-	public static bool IsEmpty(this ICollection c) => c.Count == 0;
+	public static bool IsEmpty<T>(this ICollection<T> c) => c.Count == 0;
 	
-	public static bool IsEmptyOrNull(this ICollection? c) => c == null || c.Count == 0;
+	public static bool IsEmptyOrNull<T>(this ICollection<T>? c) => c == null || c.Count == 0;
 }
 
 
