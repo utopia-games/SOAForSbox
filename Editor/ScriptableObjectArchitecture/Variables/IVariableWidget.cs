f87fd08986e397e3e58068dc182b182f748d81af
Guilhem PECH <guilhem.pech@gmail.com>
2024-05-26 15:56:50 +0200

    Rename VariableGameResourceWidget to IVariableWidget that was wrongly names. Create a VariableGameResourceWidget that specify GenericGameResourceWidget for later use

---
+++ Editor/ScriptableObjectArchitecture/Variables/IVariableWidget.cs
@@ -0,0 +1,59 @@
+﻿using Editor;
+using Sandbox.Variables;
+
+namespace Sandbox.ScriptableObjectArchitecture.Variables;
+
+
+// TODO make a custom editor for VariableGameResource that will be very similar to GameResourceWidget
+
+[CustomEditor( typeof( IVariable ) )]
+public class IVariableWidget : ControlWidget
+{
+	private TypeDescription? InternalVariableType() => TypeLibrary.GetType(SerializedProperty.GetValue<IVariable>()?.InternalTypeName);
+	
+	private IVariable? Value => SerializedProperty.GetValue<IVariable>();
+
+	public IVariableWidget(SerializedProperty property) : base(property)
+	{
+		Layout = Layout.Row();
+		Layout.Spacing = 3;
+		
+		BuildUI();
+	}
+
+	private void BuildUI()
+	{
+		var value = SerializedProperty.GetValue<IVariable>();
+
+		if ( value == null )
+		{
+			SerializedProperty.SetValue( new VariableData<float>(42) );
+		}
+		else
+		{
+			var typeProperty = value.GetType().GetProperty( nameof(VariableData<dynamic>.Value) );
+			if(typeProperty == null)
+				typeProperty = value.GetType().GetProperty( nameof(IVariable.RawValue) );
+			
+			if (typeProperty != null)
+			{
+				SerializedPropertyWrapper serializedPropertyWrapper = new SerializedPropertyWrapper( typeProperty, value, SerializedProperty.Parent);
+				var e = Create( serializedPropertyWrapper );
+				Layout.Add( e );
+			}
+		}
+	}
+
+	protected override void OnDragStart()
+	{
+		Log.Info( "start");
+
+		base.OnDragStart();
+	}
+
+	public override void OnDragDrop( DragEvent ev )
+	{
+		Log.Info( "Droped");
+		base.OnDragDrop( ev );
+	}
+}
--- Editor/ScriptableObjectArchitecture/Variables/VariableGameResourceWidget.cs
+++ Editor/ScriptableObjectArchitecture/Variables/VariableGameResourceWidget.cs
@@ -1,56 +1,12 @@
-﻿using Editor;
-using Sandbox.Variables;
+﻿using Sandbox.Variables;
 
 namespace Sandbox.ScriptableObjectArchitecture.Variables;
 
-[CustomEditor( typeof( IVariable ) )]
-public class VariableGameResourceWidget : ControlWidget
-{
-	private TypeDescription? InternalVariableType() => TypeLibrary.GetType(SerializedProperty.GetValue<IVariable>()?.InternalTypeName);
-	
-	private IVariable? Value => SerializedProperty.GetValue<IVariable>();
+[CustomEditor( typeof( VariableGameResource ) )]
 
+public class VariableGameResourceWidget : GenericGameResourceWidget
+{
 	public VariableGameResourceWidget(SerializedProperty property) : base(property)
 	{
-		Layout = Layout.Row();
-		Layout.Spacing = 3;
-		
-		BuildUI();
-	}
-
-	private void BuildUI()
-	{
-		var value = SerializedProperty.GetValue<IVariable>();
-
-		if ( value == null )
-		{
-			SerializedProperty.SetValue( new VariableData<float>(42) );
-		}
-		else
-		{
-			var typeProperty = value.GetType().GetProperty( nameof(VariableData<dynamic>.Value) );
-			if(typeProperty == null)
-				typeProperty = value.GetType().GetProperty( nameof(IVariable.RawValue) );
-			
-			if (typeProperty != null)
-			{
-				SerializedPropertyWrapper serializedPropertyWrapper = new SerializedPropertyWrapper( typeProperty, value, SerializedProperty.Parent);
-				var e = Create( serializedPropertyWrapper );
-				Layout.Add( e );
-			}
-		}
-	}
-
-	protected override void OnDragStart()
-	{
-		Log.Info( "start");
-
-		base.OnDragStart();
-	}
-
-	public override void OnDragDrop( DragEvent ev )
-	{
-		Log.Info( "Droped");
-		base.OnDragDrop( ev );
 	}
 }
